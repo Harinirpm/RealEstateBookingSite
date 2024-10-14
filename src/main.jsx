9722699bba8efc123ccd9d4031434fc9305494c5
Harinirpm <harinirangammal4@gmail.com>
2024-10-14 10:04:43 +0530

    Boiler plate setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {BrowserRouter, Routes, Route,} from 'react-router-dom';
 import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import routes from './router/Routes';
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <Routes>
+    {routes.map((route,index) => (
+     <Route key={index} path={route.path} element={route.element} />
+    ))}
+  </Routes>
+  </BrowserRouter>
 )
